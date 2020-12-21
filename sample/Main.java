de764b67a1a76b9d865f03d798900ee5e0c9603b
genielog-ue <76449744+genielog-ue@users.noreply.github.com>
2020-12-21 19:52:50 +0100

    Add files via upload

---
+++ sample/Main.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    protected Stage primaryStage; // Protected Stage to allow link to another class
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader=new FXMLLoader();
+        this.primaryStage=primaryStage; // Set the primaryStage loaded by JavaFX to the Stage var of the class
+        loader.setLocation(Main.class.getResource("FXML/Hub.fxml"));
+        Parent root = loader.load();
+        this.primaryStage.setTitle("LibBreizhRie");
+        this.primaryStage.setScene(new Scene(root, 800, 400));
+        this.primaryStage.show();
+        Controller controle=(Controller) loader.getController(); // Initialize the Controller of start page
+        controle.setMainApp(this); // Link the controller to the Main
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
