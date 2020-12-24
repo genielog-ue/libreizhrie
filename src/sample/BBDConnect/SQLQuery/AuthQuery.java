5400f60315680e4fe9bb910689b07c6f5661fd35
genielog <rom.letalour@gmail.com>
2020-12-24 17:09:51 +0100

    Add Authentification fxml/controller/Query

---
+++ src/sample/BBDConnect/SQLQuery/AuthQuery.java
@@ -0,0 +1,13 @@
+package sample.BBDConnect.SQLQuery;
+
+import sample.BBDConnect.LibBDD;
+
+import java.sql.SQLException;
+
+public class AuthQuery extends LibBDD {
+
+    public AuthQuery() throws SQLException {
+        super();
+    }
+
+}
---
+++ src/sample/Controlleurs/AuthController.java
@@ -0,0 +1,20 @@
+package sample.Controlleurs;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AuthController extends Controller{
+    public AuthController(){
+        super();
+    }
+    @FXML
+    private TextField compteField;
+    @FXML
+    private TextField mdpField;
+
+    @FXML
+    private void checkAuth(){
+
+    }
+
+}
