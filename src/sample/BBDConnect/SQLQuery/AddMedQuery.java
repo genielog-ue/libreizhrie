fa16b7970a459fb6e7ae52fa00c0b9127d886b3f
genielog <rom.letalour@gmail.com>
2020-12-25 17:34:32 +0100

    Ajout des requetes Add et Remove Usager et liées à l'application graphique

---
+++ src/sample/BBDConnect/SQLQuery/AddMedQuery.java
@@ -0,0 +1,14 @@
+package sample.BBDConnect.SQLQuery;
+
+import sample.BBDConnect.LibBDD;
+
+import java.sql.SQLException;
+
+public class AddMedQuery extends LibBDD {
+    public AddMedQuery() throws SQLException {
+        super();
+    }
+    //public AddMed(Media media){
+
+   // }
+}
