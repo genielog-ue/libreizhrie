32e5dc8c8e8d290747402449b1714602652d1d0c
genielog <rom.letalour@gmail.com>
2020-12-28 11:05:05 +0100

    Add Return Media Query

---
+++ src/sample/BBDConnect/SQLQuery/ReturnMedQuery.java
@@ -0,0 +1,20 @@
+package sample.BBDConnect.SQLQuery;
+
+import sample.BBDConnect.LibBDD;
+import sample.BBDConnect.TableClass.Media;
+import sample.BBDConnect.TableClass.Usager;
+
+import java.sql.SQLException;
+
+public class ReturnMedQuery extends LibBDD {
+    public ReturnMedQuery() throws SQLException {
+        super();
+    }
+    public void ReturnMedia(Usager user, Media media) throws SQLException {
+        String requete="DELETE FROM T_Emprunt WHERE idUsager=? and idMedia=?";
+        stmt=connect.prepareStatement(requete);
+        stmt.setInt(1,user.getIdUsager());
+        stmt.setInt(2,media.getIdMedia());
+        stmt.execute();
+    }
+}
--- src/sample/Controlleurs/MediaReturnController.java
+++ src/sample/Controlleurs/MediaReturnController.java
@@ -1,7 +1,32 @@
 package sample.Controlleurs;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import sample.BBDConnect.SQLQuery.ReturnMedQuery;
+import sample.BBDConnect.TableClass.Media;
+import sample.BBDConnect.TableClass.Usager;
+
+import java.sql.SQLException;
+
 public class MediaReturnController extends Controller {
     public MediaReturnController() {
         super();
+    }
+
+    @FXML
+    private TextField idUsagerTextField;
+    @FXML
+    private TextField idMediaTextField;
+
+    @FXML
+    private void ReturnMediaAction(ActionEvent event) throws SQLException {
+        int idUsager=Integer.parseInt(idUsagerTextField.getText());
+        int idMedia=Integer.parseInt(idMediaTextField.getText());
+        Usager user=new Usager(idUsager);
+        Media media=new Media(idMedia);
+        ReturnMedQuery query=new ReturnMedQuery();
+        query.ReturnMedia(user,media);
+
     }
 }
